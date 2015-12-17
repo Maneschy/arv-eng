c9a5164b89c80feb78899514e719ec7e79b3c712
Maneschy <thiago@maneschy.com>
2015-12-17 17:40:10 -0400

    MAIS

---
+++ portifolio_item.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8"> 
+
+  <title>ARV•ENG Engenharia Elétrica</title>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <meta name="author" content="">
+
+  <!-- Bootstrap Styles -->
+  <link href="css/bootstrap.css" rel="stylesheet">
+  <link rel="shortcut icon" href="images/ico-arv.png" type="image/x-icon" />
+  <!-- Styles -->
+  <link href="style.css" rel="stylesheet">
+  <link href="switcher/css/orange.css" rel="stylesheet">
+  <!-- Carousel Slider -->
+  <link href="css/owl-carousel.css" rel="stylesheet">
+  
+  <!-- CSS Animations -->
+  <link href="css/animate.min.css" rel="stylesheet">
+  
+  <!-- Google Fonts -->    <link href='http://fonts.googleapis.com/css?family=Raleway:100,300,600,900' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=PT+Sans:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Lato:400,300,400italic,300italic,700,700italic,900' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Exo:400,300,600,500,400italic,700italic,800,900' rel='stylesheet' type='text/css'>
+
+  <!-- SLIDER ROYAL CSS SETTINGS -->
+  <link href="royalslider/royalslider.css" rel="stylesheet">
+  <link href="royalslider/skins/default-inverted/rs-default-inverted.css" rel="stylesheet">
+  
+  <!-- SLIDER REVOLUTION 4.x CSS SETTINGS -->
+  <link rel="stylesheet" type="text/css" href="rs-plugin/css/settings.css" media="screen" />
+        
+  <!-- Support for HTML5 -->
+  <!--[if lt IE 9]>
+    <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+
+  <!-- Enable media queries on older bgeneral_rowsers -->
+  <!--[if lt IE 9]>
+    <script src="js/respond.min.js"></script>  <![endif]-->
+
+</head>
+<body>
+
+	<?php include './menu_arv.php'; ?>
+	
+   <section class="post-wrapper-top jt-shadow clearfix">   		<div class="container">   			<div class="col-lg-12">   				<h2></h2>                   <ul class="breadcrumb pull-right">                       <li><a href="index-2.html">Home</a></li>                       <li>História da ARV•ENG</li>                   </ul>   			</div>   		</div>   	</section><section class="blog-wrapper">       	<div class="container">           	<div id="content" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">                   <div class="row">                      <div class="blog-masonry">                           <div class="col-lg-12">                               <div class="blog-carousel">                                   <div class="blog-carousel-header">                                       <h1>História da ARV Engenharia</h1>                                       <div class="blog-carousel-meta">                                       </div><!-- end blog-carousel-meta -->                                   </div><!-- end blog-carousel-header -->                                   <div class="blog-carousel-desc">                                                                         									<blockquote>Gorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fermentum semper porta. Vivamus lacinia diam nec dignissim imperdiet. In purus dolor, porta ut auctor vitae, fermentum vitae ante. Duis hendrerit sed mauris eu lacinia. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fermentum semper porta. Vivamus lacinia diam nec dignissim imperdiet. In purus dolor, porta ut auctor vitae, fermentum vitae ante. Duis hendrerit sed mauris eu lacinia. </blockquote>   									                                      </div><!-- end blog-carousel-desc -->                               </div><!-- end blog-carousel -->                           </div><!-- end col-lg-12 -->   					</div><!-- end blog-masonry -->               	</div><!-- end row -->                </div><!-- end content -->       	</div><!-- end container -->       </section>
+    <section class="grey-wrapper jt-shadow nopadding" style="background-image: url(images/bg-concreto.png);">    	<div class="container">    	        	<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">    	            	<div class="widget padding-top">    	                	<h3>BEM-VINDO A<span> <b>ARV•ENG</b></h3>    	                   <br>    	                    <div id="accordion-first" class="clearfix">    	                        <div class="accordion" id="accordion2">    	                          <div class="accordion-group">    	                            <div class="accordion-heading">    	                              <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseSix">    	                                <em class="fa fa-plus icon-fixed-width"></em><b>MISSÃO</b>    	                              </a>    	                            </div>    	                            <div id="collapseSix" class="accordion-body collapse">    	                              <div class="accordion-inner">    	                                Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga.    	                              </div>    	                            </div>    	                          </div>    	                          <div class="accordion-group">    	                            <div class="accordion-heading">    	                              <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseSeven">    	                                <em class="fa fa-plus icon-fixed-width"></em><b>VISÃO</b>    	                              </a>    	                            </div>    	                            <div id="collapseSeven" class="accordion-body collapse">    	                              <div class="accordion-inner">    	                                Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga.    	                              </div>    	                            </div>    	                          </div>    	    	                          <div class="accordion-group">    	                            <div class="accordion-heading">    	                              <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseTen">    	                                <em class="fa fa-plus icon-fixed-width"></em><b>VALORES</b>    	                              </a>    	                            </div>    	                            <div id="collapseTen" class="accordion-body collapse">    	                              <div class="accordion-inner">    	                                Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga.    	                              </div>    	                            </div>    	                          </div>      	                        </div><!-- end accordion -->    	                    </div><!-- end accordion first -->    	                </div><!-- end widget -->    	            </div><!-- end col-lg-6 -->    	                	        	<div class="col-lg-5 col-md-5 col-sm-5 col-xs-12">    	            	<div class="wow fadeInRight margin-top">    	            	    <br/><br/><br/>    						<img class="img-responsive" src="images/sliderman_02.png" alt="">    	                </div>    				</div><!-- end col-lg-6 -->    			</div><!-- end container -->    </section>
+	<section class="white-wrapper">		<div class="container">			<div class="general-title">	        	<h2>Olá! somos a ARV•ENG</h2>	            <hr>	            <p class="lead">Conheça a equipe de profissionais que lidera a família ARV ENGENHARIA</p>	        </div><!-- end general title -->	        	        <div class="padding-top text-center">	        	<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 first">					<div class="team_member">	                	<div class="entry">	                		<img class="img-responsive" src="demos/02_team.png" alt="">	                        <div class="magnifier">	                            <div class="buttons">	                                <a class="sf" rel="bookmark" href="#"><span class="fa fa-envelope"></span></a>	                                <a class="sg" rel="bookmark" href="#"><span class="fa fa-facebook"></span></a>	                            </div>	                        </div><!-- end magnifier -->	                    </div><!-- end entry -->	                    <h3>ALAN BASTOS <span>|</span> <small>Engenheiro</small></h3>	                    <p>Cras dapibus. Vivamus elementum semper  Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae</p>					</div><!-- end team_member -->	            </div><!-- end col-lg-3 -->	        	<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">					<div class="team_member">	                	<div class="entry">	                		<img class="img-responsive" src="demos/03_team.png" alt="">	                        <div class="magnifier">	                            <div class="buttons">	                                <a class="sf" rel="bookmark" href="#"><span class="fa fa-envelope"></span></a>	                                <a class="sg" rel="bookmark" href="#"><span class="fa fa-facebook"></span></a>	                            </div>	                        </div><!-- end magnifier -->	                    </div><!-- end entry -->	                    <h3>MÁRIO KASUO <span>|</span> <small>Engenheiro</small></h3>	                    <p>Cras dapibus. Vivamus elementum semper  Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae</p> 					</div><!-- end team_member -->	            </div><!-- end col-lg-3 -->	        	<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">					<div class="team_member">	                	<div class="entry">	                		<img class="img-responsive" src="demos/01_team.png" alt="">	                        <div class="magnifier">	                            <div class="buttons">	                                <a class="sf" rel="bookmark" href="#"><span class="fa fa-google-plus"></span></a>	                                <a class="sg" rel="bookmark" href="#"><span class="fa fa-facebook"></span></a>	                            </div>	                        </div><!-- end magnifier -->	                    </div><!-- end entry -->	                    <h3>JÉSSICA ARRUDA <span>|</span> <small>Secretária ADM</small></h3>	                    <p>Cras dapibus. Vivamus elementum semper  Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae</p>					</div><!-- end team_member -->	            </div><!-- end col-lg-3 -->	        	<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 last">					<div class="team_member">	                	<div class="entry">	                		<img class="img-responsive" src="demos/04_team.png" alt="">	                        <div class="magnifier">	                            <div class="buttons">	                                <a class="sf" rel="bookmark" href="#"><span class="fa fa-envelope"></span></a>	                                <a class="sg" rel="bookmark" href="#"><span class="fa fa-facebook"></span></a>	                            </div>	                        </div><!-- end magnifier -->	                    </div><!-- end entry -->	                    <h3>KLEBER MOURÃO  <span>|</span> <small>Comercial</small></h3>	                    <p>Cras dapibus. Vivamus elementum semper  Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae</p>					</div><!-- end team_member -->	            </div><!-- end col-lg-3 -->	        </div><!-- end row -->   		</div><!-- end container -->	</section>
+    
+	<!-- end #footer-style-1 -->    
+
+<div id="copyrights" style="background-color: #1C75BC; color: #fff; font-size: 12px;">
+    	<div class="container">
+			<div class="col-lg-7 col-md-6 col-sm-12 ">			<img src="images/logo-arv-footer.png" width="25%" alt=""></a>
+				<!--<div class="footer-menu">
+                    <ul class="menu">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">Headers</a></li>
+                        <li><a href="#">Portfolio</a></li>
+                        <li><a href="#">Features</a></li>
+                        <li><a href="contact.html">Contact</a></li>
+                    </ul>
+                </div>-->
+			</div><!-- end large-7 --> 							<div class="col-lg-5 col-md-6 col-sm-12 clearfix">			    	<div class="copyright-text">			            <p>Copyright © 2016 • Todos os direitos reservados • by <a href="#" target="_blank">Maneschy.com</a> </p>			        </div><!-- end copyright-text -->				</div><!-- end widget -->
+        </div><!-- end container -->
+    </div><!-- end copyrights -->
+    
+	<div class="dmtop">voltar ao topo</div>
+        
+  <!-- Main Scripts-->
+  <script src="js/jquery.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/menu.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.parallax-1.1.3.js"></script>
+  <script src="js/jquery.simple-text-rotator.js"></script>
+  <script src="js/wow.min.js"></script>  <script src="js/jquery.fitvids.js"></script>
+  <script src="js/custom.js"></script>
+  
+  <script src="js/jquery.isotope.min.js"></script>
+  <script src="js/custom-portfolio.js"></script>
+
+  <!-- SLIDER REVOLUTION 4.x SCRIPTS  -->
+  <script type="text/javascript" src="rs-plugin/js/jquery.themepunch.plugins.min.js"></script>
+  <script type="text/javascript" src="rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+  <script type="text/javascript">
+	var revapi;
+	jQuery(document).ready(function() {
+		revapi = jQuery('.tp-banner').revolution(
+		{
+			delay:9000,
+			startwidth:1170,
+			startheight:500,
+			hideThumbs:10,
+			fullWidth:"on",
+			forceFullWidth:"on"
+		});
+	});	//ready
+  </script>
+               <!-- Affix menu -->        <script>      (function($) {        "use strict";      		$("#header-style-1").affix({      		offset: {      		  top: 100      		, bottom: function () {      			return (this.bottom = $('#copyrights').outerHeight(true))      		  }      		}      	})      })(jQuery);      </script>      
+  <!-- Royal Slider script files -->
+  <script src="royalslider/jquery.easing-1.3.js"></script>
+  <script src="royalslider/jquery.royalslider.min.js"></script>
+  <script>
+	jQuery(document).ready(function($) {
+	  var rsi = $('#slider-in-laptop').royalSlider({
+		autoHeight: false,
+		arrowsNav: false,
+		fadeinLoadedSlide: false,
+		controlNavigationSpacing: 0,
+		controlNavigation: 'bullets',
+		imageScaleMode: 'fill',
+		imageAlignCenter: true,
+		loop: false,
+		loopRewind: false,
+		numImagesToPreload: 6,
+		keyboardNavEnabled: true,
+		autoScaleSlider: true,  
+		autoScaleSliderWidth: 486,     
+		autoScaleSliderHeight: 315,
+	
+		/* size of all images http://help.dimsemenov.com/kb/royalslider-jquery-plugin-faq/adding-width-and-height-properties-to-images */
+		imgWidth: 792,
+		imgHeight: 479
+	
+	  }).data('royalSlider');
+	  $('#slider-next').click(function() {
+		rsi.next();
+	  });
+	  $('#slider-prev').click(function() {
+		rsi.prev();
+	  });
+	});
+  </script>
+    
+    
+</body>
+</html>
