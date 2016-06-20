f1761955bbdf5a6742bd4c884af97460abf59402
Thiago Maneschy <thiago@maneschy.com>
2016-06-20 12:00:12 -0400

    vaia

---
+++ Cópia de portifolio.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8"> 
+
+  <title>ARV•ENG Engenharia Elétrica</title>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <meta name="author" content="">
+
+  <!-- Bootstrap Styles -->
+  <link href="css/bootstrap.css" rel="stylesheet">
+  <link rel="shortcut icon" href="images/ico-arv.png" type="image/x-icon" />
+  <!-- Styles -->
+  <link href="style.css" rel="stylesheet">
+  <link href="switcher/css/orange.css" rel="stylesheet">
+  <!-- Carousel Slider -->
+  <link href="css/owl-carousel.css" rel="stylesheet">
+  
+  <!-- CSS Animations -->
+  <link href="css/animate.min.css" rel="stylesheet">
+  
+  <!-- Google Fonts -->    <link href='http://fonts.googleapis.com/css?family=Raleway:100,300,600,900' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=PT+Sans:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Lato:400,300,400italic,300italic,700,700italic,900' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Exo:400,300,600,500,400italic,700italic,800,900' rel='stylesheet' type='text/css'>
+
+  <!-- SLIDER ROYAL CSS SETTINGS -->
+  <link href="royalslider/royalslider.css" rel="stylesheet">
+  <link href="royalslider/skins/default-inverted/rs-default-inverted.css" rel="stylesheet">
+  
+  <!-- SLIDER REVOLUTION 4.x CSS SETTINGS -->
+  <link rel="stylesheet" type="text/css" href="rs-plugin/css/settings.css" media="screen" />
+        
+  <!-- Support for HTML5 -->
+  <!--[if lt IE 9]>
+    <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+
+  <!-- Enable media queries on older bgeneral_rowsers -->
+  <!--[if lt IE 9]>
+    <script src="js/respond.min.js"></script>  <![endif]-->
+
+</head>
+<body>
+
+	<?php include './menu_arv.php'; ?>
+	
+   <section class="grey-wrapper jt-shadow nopadding" style="background-image: url(images/bg-concreto.png);">       	<div class="container" style="background-image: url(images/bg-concreto.png);">           	<div class="custom-message">           	<h1>Conheça nos cases de sucesso!</h1>               </div>           </div><!-- end container -->      </section>    <section class="white-wrapper">       	<div class="container">           	<div class="general-title">                   <hr>               </div><!-- end general title -->   		</div><!-- end container -->      		<div class="text-center clearfix">   			<nav class="portfolio-filter">                   <ul style="text-transform: uppercase;">                       <li><a style=" font-weight: 600;" class="btn btn-primary" href="#" data-filter="*"><span></span>tudo</a></li>                       <li><a style=" font-weight: 600;" class="btn btn-primary" href="#" data-filter=".projetos">Projetos</a></li>                       <li><a style=" font-weight: 600;" class="btn btn-primary" href="#" data-filter=".distribuicao">Painéis Distribuição</a></li>                       <li><a style=" font-weight: 600;" class="btn btn-primary" href="#" data-filter=".comando">Painéis Comando</a></li>                       <li><a style=" font-weight: 600;" class="btn btn-primary" href="#" data-filter=".subestacao">Subestações</a></li>                       <li><a style=" font-weight: 600;" class="btn btn-primary" href="#" data-filter=".diversos">Diversos</a></li>                   </ul>   			</nav>   		</div><!-- end text-center -->                   		<div class="portfolio_wrapper padding-top">   			<div class="portfolio_item photography">   				<div class="entry">   					<img src="demos/portfolio_01.jpg" alt="" class="img-responsive">   					<div class="magnifier">   						<div class="buttons">   							<img src="images/logo-arv-branca.png" alt="" />   							<a class="st btn btn-default" rel="bookmark" href="portifolio_item.php">VER PROJETO</a>                               <h3>Subestação SANSUNG</h3>   						</div><!-- end buttons -->   					</div><!-- end magnifier -->   				</div><!-- end entry -->   			</div><!-- end portfolio_item -->      			<div class="portfolio_item photography app-design">   				<div class="entry">   					<img src="demos/portfolio_02.jpg" alt="" class="img-responsive">   					<div class="magnifier">   						<div class="buttons">   								<img src="images/logo-arv-branca.png" alt="" />   								<a class="st btn btn-default" rel="bookmark" href="portifolio_item.php">VER PROJETO</a>   						       <h3>Subestação SANSUNG</h3>   							</div><!-- end buttons -->   					</div><!-- end magnifier -->   				</div><!-- end entry -->   			</div><!-- end portfolio_item -->                  			<div class="portfolio_item app-design">   				<div class="entry">   					<img src="demos/portfolio_03.jpg" alt="" class="img-responsive">   					<div class="magnifier">   						<div class="buttons">   								<img src="images/logo-arv-branca.png" alt="" />   								<a class="st btn btn-default" rel="bookmark" href="portifolio_item.php">VER PROJETO</a>   						       <h3>Subestação SANSUNG</h3>   							</div><!-- end buttons -->   					</div><!-- end magnifier -->   				</div><!-- end entry -->   			</div><!-- end portfolio_item -->                  			<div class="portfolio_item app-design">   				<div class="entry">   					<img src="demos/portfolio_04.jpg" alt="" class="img-responsive">   					<div class="magnifier">   						<div class="buttons">   								<img src="images/logo-arv-branca.png" alt="" />   								<a class="st btn btn-default" rel="bookmark" href="portifolio_item.php">VER PROJETO</a>   						       <h3>Subestação SANSUNG</h3>   							</div><!-- end buttons -->   					</div><!-- end magnifier -->   				</div><!-- end entry -->   			</div><!-- end portfolio_item -->              			<div class="portfolio_item photography">   				<div class="entry">   					<img src="demos/portfolio_11.jpg" alt="" class="img-responsive">   					<div class="magnifier">   						<div class="buttons">   								<img src="images/logo-arv-branca.png" alt="" />   								<a class="st btn btn-default" rel="bookmark" href="portifolio_item.php">VER PROJETO</a>   						       <h3>Subestação SANSUNG</h3>   							</div><!-- end buttons -->   					</div><!-- end magnifier -->   				</div><!-- end entry -->   			</div><!-- end portfolio_item -->      			<div class="portfolio_item logo">   				<div class="entry">   					<img src="demos/portfolio_12.jpg" alt="" class="img-responsive">   					<div class="magnifier">   						<div class="buttons">   								<img src="images/logo-arv-branca.png" alt="" />   								<a class="st btn btn-default" rel="bookmark" href="portifolio_item.php">VER PROJETO</a>   						       <h3>Subestação SANSUNG</h3>   							</div><!-- end buttons -->   					</div><!-- end magnifier -->   				</div><!-- end entry -->   			</div><!-- end portfolio_item -->                  			<div class="portfolio_item logo">   				<div class="entry">   					<img src="demos/portfolio_13.jpg" alt="" class="img-responsive">   					<div class="magnifier">   						<div class="buttons">   								<img src="images/logo-arv-branca.png" alt="" />   								<a class="st btn btn-default" rel="bookmark" href="portifolio_item.php">VER PROJETO</a>   						       <h3>Subestação SANSUNG</h3>   							</div><!-- end buttons -->   					</div><!-- end magnifier -->   				</div><!-- end entry -->   			</div><!-- end portfolio_item -->                  			<div class="portfolio_item logo">   				<div class="entry">   					<img src="demos/portfolio_14.jpg" alt="" class="img-responsive">   					<div class="magnifier">   						<div class="buttons">   								<img src="images/logo-arv-branca.png" alt="" />   								<a class="st btn btn-default" rel="bookmark" href="portifolio_item.php">VER PROJETO</a>   						       <h3>Subestação SANSUNG</h3>   							</div><!-- end buttons -->   					</div><!-- end magnifier -->   				</div><!-- end entry -->   			</div><!-- end portfolio_item -->   		</div><!-- portfolio_wrapper -->              		<div class="clearfix"></div>                      </section><!-- end #footer-style-1 -->              <div id="copyrights" style="background-color: #1C75BC; color: #fff; font-size: 12px;">           	<div class="container">       			<div class="col-lg-7 col-md-6 col-sm-12 ">       			<img src="images/logo-arv-footer.png" width="25%" alt=""></a>       				<!--<div class="footer-menu">                           <ul class="menu">                               <li class="active"><a href="#">Home</a></li>                               <li><a href="#">About us</a></li>                               <li><a href="#">Headers</a></li>                               <li><a href="#">Portfolio</a></li>                               <li><a href="#">Features</a></li>                               <li><a href="contact.html">Contact</a></li>                           </ul>                       </div>-->       			</div><!-- end large-7 -->        			       				<div class="col-lg-5 col-md-6 col-sm-12 clearfix">       			    	<div class="copyright-text">       			            <p>Copyright © 2016 • Todos os direitos reservados • by <a href="#" target="_blank">Maneschy.com</a> </p>       			        </div><!-- end copyright-text -->       				</div><!-- end widget -->               </div><!-- end container -->           </div>           <div class="dmtop">Scroll to Top</div>                <!-- Main Scripts-->     <script src="js/jquery.js"></script>     <script src="js/bootstrap.min.js"></script>     <script src="js/menu.js"></script>     <script src="js/owl.carousel.min.js"></script>     <script src="js/jquery.parallax-1.1.3.js"></script>     <script src="js/jquery.simple-text-rotator.js"></script>     <script src="js/wow.min.js"></script>     <script src="js/custom.js"></script>     <script src="js/jquery.isotope.min.js"></script>     <script src="js/custom-portfolio.js"></script>
